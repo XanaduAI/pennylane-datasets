07f0942ac5efdbe9d149ed5d4f0de774ed6f224b
Jack Brown <jack@xanadu.ai>
2024-04-30 10:18:18 -0400

    indent

--- lib/dsets/lib/json_fmt.py
+++ lib/dsets/lib/json_fmt.py
@@ -2,12 +2,13 @@ import json
 from pathlib import Path
 
 
-def format(path: Path | str, check: bool) -> bool:
+def format(path: Path | str, check: bool, *, indent: int = 2) -> bool:
     """Format JSON file at ``path``.
 
     Args:
         path: Path to JSON file
         check: If True, do not write modified file
+        indent: Indentation level, defaults to 2
 
     Returns:
         True: if file was not correctly formatted
@@ -16,7 +17,7 @@ def format(path: Path | str, check: bool) -> bool:
     with open(path, "r", encoding="utf-8") as f:
         raw_before = f.read()
 
-    raw_after = json.dumps(json.loads(raw_before), indent=2)
+    raw_after = json.dumps(json.loads(raw_before), indent=indent)
     if raw_before == raw_after:
         return False
 
