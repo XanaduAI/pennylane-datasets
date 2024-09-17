a56e7d9f7908f3ef777ce3096dc8829a2c5e405b
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2024-09-17 16:20:20 -0400

    Add `Author`, `AuthorName`, and `AuthorHandle` schemas

--- lib/dsets/schemas/__init__.py
+++ lib/dsets/schemas/__init__.py
@@ -1,9 +1,13 @@
+from .author import Author, AuthorHandle, AuthorName
 from .dataset import Dataset
 from .dataset_class import DatasetAttribute, DatasetClass, DatasetParameter
 from .dataset_collection import DatasetCollection
 from .dataset_family import DatasetFamily, DatasetFamilyMeta, DatasetFeature
 
 __all__ = [
+    "Author",
+    "AuthorHandle",
+    "AuthorName",
     "Dataset",
     "DatasetAttribute",
     "DatasetCollection",
